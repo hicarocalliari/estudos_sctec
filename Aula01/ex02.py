b23dae06f521c2b20002a920b31e9ea9006a27a5
Hicaro Calliari Bottenberg <hicaro.bottenberg@gmail.com>
2026-04-24 21:13:38 -0300

    aula do dia 24

---
+++ Aula01/ex02.py
@@ -0,0 +1,14 @@
+for num in range(1,21):
+    print (num)
+
+for num in range(10):
+    print (f"Olá mundo: {num}")
+
+for num in range(0,101,2):
+    print (f"Atual: {num}")
+
+for num in range (0,5):
+    valorTeste = float(input("Digite o Numero: "))
+    if valorTeste > 10:
+        print (f"O valor foi maior que 10, e o valor digitado foi: {valorTeste}")
+     
---
+++ Aula01/main.py
@@ -0,0 +1,33 @@
+a = 15
+b = 21
+21
+if (a > b):
+    print ("A é manor")
+else:
+    print ("B é maior")
+
+########
+c = int (input ("Digite o Valor: "))
+
+if ( c>= 0):
+    print ("Valor é positivo")
+else:
+    print ("valor negativo")
+
+#########
+
+d = int (input ("Digite o Valor: "))
+
+if ( d > 100):
+    print ("Valor é maior que 100")
+else:
+    print ("Valor não é maior")
+
+#########
+
+f = input ("Digite o Sexo: M/F: ")
+
+if ( f == "M" or f =="F"):
+    print ("Pode passar")
+else:
+    print ("Sexo inserido inválido")
